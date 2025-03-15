8994108662dd5ff89a9a5f3c6fa1db3a38410ddd
AksilDev <axcelsonmacansantos@gmail.com>
2025-03-15 23:29:39 +0800

    movement frams

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -70,6 +70,15 @@ public class Player extends Entity{
             direction ="right";
             x += speed;
         }
+        spriteCounter++;
+        if(spriteCounter > 10){
+            if(spriteNum == 1){
+                spriteNum = 2;
+            }else if(spriteNum == 2){
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
     }
     public void draw(Graphics2D g2){
 //        g2.setColor(Color.white);
@@ -78,10 +87,70 @@ public class Player extends Entity{
         BufferedImage image = null;
 
         switch(direction){
-            case "up": image = up1; break;
-            case "down": image = down1; break;
-            case "left": image = left1; break;
-            case "right": image = right1; break;
+            case "up":
+            if(spriteNum == 1){
+                image = up1;
+            }
+            if(spriteNum == 2){
+                image = up2;
+            }
+            if(spriteNum == 3){
+                image = up3;
+            }
+            if(spriteNum == 4){
+                image = up4;
+            }
+            break;
+
+
+
+            case "down":
+            if(spriteNum == 1){
+                image = down1;
+            }
+            if(spriteNum == 2){
+                image = down2;
+            }
+            if(spriteNum == 3){
+                image = down3;
+            }
+            if(spriteNum == 4){
+                image = down4;
+            }
+            break;
+
+
+            case "left":
+            if(spriteNum == 1){
+                image = left1;
+            }
+            if(spriteNum == 2){
+                image = left2;
+            }
+            if(spriteNum == 3){
+                image = left3;
+            }
+            if(spriteNum == 4){
+                image = left4;
+            }
+            break;
+
+
+
+            case "right":
+            if(spriteNum == 1){
+                image = right1;
+            }
+            if(spriteNum == 2){
+                image = right2;
+            }
+            if(spriteNum == 3){
+                image = right3;
+            }
+            if(spriteNum == 4){
+                image = right4;
+            }
+            break;
         }
             g2.drawImage(image, x,y, gp.tileSize, gp.tileSize, null);
     }
