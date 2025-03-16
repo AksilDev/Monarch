b2d6c4a414e4c28d64091417bb71f4163e38fc6d
AksilDev <axcelsonmacansantos@gmail.com>
2025-03-16 23:16:13 +0800

    temp

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -20,12 +20,19 @@ public final int tileSize = originalTileSize * scale; // 48x48 tile
     public final int screenWidth = tileSize * maxScreenCol;
     public final int screenHeight = tileSize * maxScreenRow;
 
+    //WORLD SETTINGS
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
+
     int FPS = 60;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new  Player(this,keyH);
+    public Player player = new  Player(this,keyH);
 
     //static position of char // not used
 
