2b48cf13b32a4f184c3f048d27659cf795b3e34f
AksilDev <axcelsonmacansantos@gmail.com>
2025-03-16 21:07:21 +0800

    FTYpxMAP

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import tile.TileManager;
 
 import java.awt.*;
 import javax.swing.JPanel;
@@ -12,23 +13,22 @@ final int scale = 3;
 
 
 public final int tileSize = originalTileSize * scale; // 48x48 tile
-final int maxScreenCol = 33;
-final int maxScreenRow = 20;
-//    final int maxScreenCol = 16;
-//    final int maxScreenRow = 12;
-final int screenWidth = tileSize * maxScreenCol; // 1584 pixels
-final int screenHeight = tileSize * maxScreenRow; // 960 pixels
+//    public final int maxScreenCol = 33;       // 1584px
+//    public final int maxScreenRow = 20;       // 960px
+    public final int maxScreenCol = 16;         //768px
+    public final int maxScreenRow = 12;         //576px
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
 
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new  Player(this,keyH);
 
-    //static position of char
-    int playerX = 50;
-    int playerY = 50;
-    int playerSpeed = 4;
+    //static position of char // not used
+
 
         public GamePanel() {
             this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -75,6 +75,7 @@ final int screenHeight = tileSize * maxScreenRow; // 960 pixels
             super.paintComponent(g);
             Graphics2D g2 = (Graphics2D)g;
 
+            tileM.draw(g2);
             player.draw(g2);
 
             g2.dispose();
