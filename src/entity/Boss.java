76cff299bba2b54ebab71918e63811450019b0a3
AksilDev <axcelsonmacansantos@gmail.com>
2025-03-25 00:55:47 +0800

    Kill and be Killed

---
+++ src/entity/Boss.java
@@ -0,0 +1,11 @@
+//public class Boss extends Enemy {
+//    public Boss(GamePanel gp, int x, int y) {
+//        super(gp);
+//        this.worldX = x;
+//        this.worldY = y;
+//        this.maxHP = 20;
+//        this.currentHP = 20;
+//        this.attack = 5;
+//        // load different sprite, override update logic for bigger AI
+//    }
+//}
